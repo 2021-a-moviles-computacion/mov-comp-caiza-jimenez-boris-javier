c49a1c6bd12b2e81540ec32841dfc7173844c286
Boris Caiza <boris.caiza@epn.edu.ec>
2021-08-30 12:00:16 -0500

    Firebase

---
+++ 03-firebase/app/src/main/java/com/example/firebaseuno/BUsuarioFirebase.kt
@@ -0,0 +1,7 @@
+package com.example.firebaseuno
+
+class BUsuarioFirebase(val uid: String, val email: String, val roles: ArrayList<String>) {
+
+
+
+}
--- 03-firebase/app/src/main/java/com/example/firebaseuno/MainActivity.kt
+++ 03-firebase/app/src/main/java/com/example/firebaseuno/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.firebaseuno
 
 import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import com.example.firebaseuno.dto.FirestoreUsuarioDto
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
-import com.google.android.gms.auth.api.Auth
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +25,11 @@ class MainActivity : AppCompatActivity() {
         val botonLogin = findViewById<Button>(R.id.btn_login)
         botonLogin.setOnClickListener{
                 llamarLoginUsuario()
+        }
+
+        val botonLogout = findViewById<Button>(R.id.btn_logout)
+        botonLogout.setOnClickListener{
+            solicitarSalirDelAplicativo()
         }
     }
 
@@ -47,17 +56,108 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode){
             codigo_inicio_sesion-> {
-                if(resultCode == Activity.RESULT_OK){
+                if(resultCode == Activity.RESULT_OK) {
                     val usuario = IdpResponse.fromResultIntent(data)
-                    if(usuario?.isNewUser == true){
-                        Log.i("firebase-login","Nuevo Usuario")
-                    }else{
-                        Log.i("firebase-login","Usario Antiguo")
+                    if (usuario != null) {
+                        if (usuario?.isNewUser == true) {
+                            Log.i("firebase-login", "Nuevo Usuario")
+                            registrarUsuarioPorPrimeraVez(usuario)
+                        } else {
+                            setearUsuarioFirebase()
+                            Log.i("firebase-login", "Usario Antiguo")
+
+
+                        }
+                      }
+                    } else {
+                        Log.i("firebase-login", "El usuario cancelo")
                     }
-                }else{
-                    Log.i("firebase-login","El usuario cancelo")
+            }
+        }
+    }
+
+    fun setearUsuarioFirebase(){
+        val instanciaAuth = FirebaseAuth.getInstance()
+        val usuarioLocal = instanciaAuth.currentUser
+        if(usuarioLocal != null){
+            if(usuarioLocal.email != null){
+                val db = Firebase.firestore
+                val referencia = db.collection("usuario").document(usuarioLocal.email.toString())
+                referencia.get().addOnSuccessListener {
+                    val usuarioCargado: FirestoreUsuarioDto?  = it.toObject(FirestoreUsuarioDto::class.java)
+
+                    if(usuarioCargado !=null){
+                        BAuthUsuario.usuario = BUsuarioFirebase(usuarioCargado.uid, usuarioCargado.email,
+                        usuarioCargado.roles)
+                    }
+                    setearBienvenida()
+
+                   /* Log.i("firebase-firestore","${usuarioCargado!!.email}")
+                    Log.i("firebase-firestore","${usuarioCargado!!.uid}")
+                    Log.i("firebase-firestore","${usuarioCargado!!.roles}")*/
+
+
+
+
+                    Log.i("firebase-firestore","Usuario cargado")
+                }.addOnFailureListener{
+                    Log.i("firebase-firestore","Fallo cargar usuario")
                 }
             }
         }
     }
+
+    fun registrarUsuarioPorPrimeraVez(usuario:IdpResponse){
+        val identificadorUsuario  = usuario.email
+        val  usuarioLogeado = FirebaseAuth.getInstance().getCurrentUser()
+        if(usuario.email !=null && usuarioLogeado !=null){
+            //roles : ["usuario", "admin"]
+            val db = Firebase.firestore
+            val rolesUsuario = arrayListOf("usuario")
+            val nuevoUsuario = hashMapOf<String, Any>(
+                "roles" to rolesUsuario,
+                "uid" to usuarioLogeado.uid,
+                "email" to identificadorUsuario.toString()
+            )
+
+            db.collection("usuario").document(identificadorUsuario.toString())
+                .set(nuevoUsuario).addOnSuccessListener {
+              Log.i("firebase-firestore","Se creo")
+                    setearUsuarioFirebase()
+            }
+                .addOnFailureListener{
+                    Log.i("firebase-firestore","Fallo")
+                }
+
+
+        }else{
+            Log.i("firebase-login","ERROR")
+        }
+    }
+
+        fun setearBienvenida(){
+            val textViewBienvenida = findViewById<TextView>(R.id.tv_bienvenida)
+            val botonLogin = findViewById<Button>(R.id.btn_login)
+            val botonLogout = findViewById<Button>(R.id.btn_logout)
+            if(BAuthUsuario.usuario != null){
+                textViewBienvenida.text = "Bienvenido ${BAuthUsuario.usuario?.email}"
+                botonLogin.visibility = View.INVISIBLE
+                botonLogout.visibility = View.VISIBLE
+            }else{
+                textViewBienvenida.text = "Ingresa al aplicativo"
+                botonLogin.visibility = View.VISIBLE
+                botonLogout.visibility = View.INVISIBLE
+            }
+        }
+
+
+    fun solicitarSalirDelAplicativo(){
+        AuthUI.getInstance().signOut(this).addOnCompleteListener{
+            BAuthUsuario.usuario = null
+            setearBienvenida()
+        }
+    }
+
+
+
 }
